38169dc38fcd9e7cbc3d9138650fcf1da3ee7bf9
davis.abols <davis.abols@gandrs.lv>
2020-07-04 20:26:34 +0300

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'verify';
+    protected $redirectTo = '/profils';
 
     /**
      * Create a new controller instance.
