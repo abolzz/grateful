5969f1f2cff10cd4212ef5bd13a033e2e124b566
davis.abols <davis.abols@gandrs.lv>
2020-07-07 20:37:00 +0300

    Update livesearch.php

--- public/livesearch.php
+++ public/livesearch.php
@@ -4,7 +4,7 @@
   use Dotenv\Dotenv;
 
   $search = $_GET["search"];
-  $link = mysqli_connect(env('DB_HOST', '127.0.0.1'), env('DB_USERNAME', 'forge'), env('DB_PASSWORD', '')) OR die (mysqli_error());
+  $link = mysqli_connect(env('DB_HOST', '127.0.0.1'), env('DB_USERNAME', 'forge'), env('DB_PASSWORD', ''), env('DB_DATABASE', '')) OR die (mysqli_error());
   mysqli_select_db ($link, "grateful") or die(mysqli_error());
 
   $query = "SELECT * FROM `shops` WHERE `name` LIKE '$search%' ORDER BY `id` DESC";
