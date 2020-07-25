2a58d589ffd6174bfdc14944a2ee4f1191c9ffd5
abolzz <abolzzy@gmail.com>
2020-07-25 18:13:40 +0300

    Update db.php

--- public/config/db.php
+++ public/config/db.php
@@ -1,6 +1,6 @@
 <?php
   // DB Params
-  define("DB_HOST", "localhost");
-  define("DB_USER", "root");
-  define("DB_PASS", "");
-  define("DB_NAME", "grateful");
+  define("DB_HOST", "DB_HOST");
+  define("DB_USER", "DB_USERNAME");
+  define("DB_PASS", "DB_PASSWORD");
+  define("DB_NAME", "DB_DATABASE");
