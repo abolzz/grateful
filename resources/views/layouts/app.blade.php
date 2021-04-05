24a654b71927ac295705cad5e5109292a0e60337
abolzz <abolzzy@gmail.com>
2021-04-05 14:57:30 +0300

    cookie banner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,17 @@
                       <div class="lds-ripple"><div></div><div></div></div>
                     </div>
 
+                    <div class="d-none" id="cookie-banner">
+                        <div>
+                             <span>
+                                    Šī vietne izmanto sīkdatnes (cookies). Lietojot to, Jūs piekrītat to izmantošanai.
+                             </span>
+                             <span id="consent-cookies">
+                                    Aizvērt
+                             </span>
+                        </div>
+                    </div>
+
                 @yield('content')
                 @yield('scripts')
 
@@ -38,9 +49,51 @@
 
                   $(".loader-bg").fadeOut("slow");
 
+                    function getCookie(cname) {
+                        const name = cname + "=";
+                        const decodedCookie = decodeURIComponent(document.cookie);
+                        const ca = decodedCookie.split(';');
+
+                        for(let i = 0; i < ca.length; i++) {
+                            let c = ca[i];
+                            while (c.charAt(0) === ' ') {
+                                c = c.substring(1);
+                            }
+                            if (c.indexOf(name) === 0) {
+                                return c.substring(name.length, c.length);
+                            }
+                        }
+
+                        return "";
+                    }
+
+                    function setCookie(cname, cvalue, exdays) {
+                        const d = new Date();
+
+                        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+
+                        const expires = "expires="+ d.toUTCString();
+
+                        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+                    }
+
+                    const cookieBanner = document.querySelector('#cookie-banner');
+                    const hasCookieConsent = getCookie('cookies-consent');
+
+                    if (!hasCookieConsent) {
+                        cookieBanner.classList.remove('d-none');
+                    }
+
+                    const consentCta = cookieBanner.querySelector('#consent-cookies');
+
+                    consentCta.addEventListener('click', () => {
+                        cookieBanner.classList.add('d-none');
+                        setCookie('cookies-consent', 1, 365);
+                    });
+
                 });
             </script>
-            
+
         </main>
         @include('includes.footer')
     </body>
